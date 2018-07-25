adcae8aa9d87fd9b30690eecfb737a570bf54697
Bu66leSort <38589128+Bu66leSort@users.noreply.github.com>
2018-07-25 16:16:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lineika
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Your coefficients?");
+            string a = Console.ReadLine(), b = Console.ReadLine(), c = Console.ReadLine();
+            SolutionOfEquation soe = new SolutionOfEquation(ref a, ref b, ref c);
+            double[] answers = new double[2];
+            answers = soe.Solution(a, b, c);
+            Console.WriteLine();
+            if (answers != null)
+            {
+                Console.WriteLine("x1 = {0}\nx2 = {1}", answers[0].ToString(), answers[1].ToString());
+            }
+        }
+    }
+}
